19709f06b32b0370d043a17042612e504e096a35
Caleb Rowland <cjrowland4@gmail.com>
2024-06-11 16:23:25 -0500

    Add stage 3 of the chat bot

--- chat-bot.js
+++ chat-bot.js
@@ -1,7 +1,7 @@
 const input = require('sync-input');
 
 console.log("Hello! My name is Aid.");
-console.log("I was created in 2020.");
+console.log("I was created in 2023.");
 console.log("Please, remind me your name.");
 
 let name = input();
@@ -10,6 +10,12 @@ console.log("What a great name you have, " + name + "!");
 console.log("Let me guess your age.");
 console.log("Enter remainders of dividing your age by 3, 5 and 7.");
 
+let remainder3 = Number(input());
+let remainder5 = Number(input());
+let remainder7 = Number(input());
+
+let age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
 // reading all remainders
 
-console.log("Your age is {your_age}; that's a good time to start programming!");
+console.log("Your age is " + age + "; that's a good time to start programming!");
